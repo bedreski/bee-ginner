e64b80145e82662cc0527f80eb7507fcbc34b1d1
bedreski <fabianabedreski@gmail.com>
2024-10-29 09:27:20 -0300

    1016 solved

--- C++/1016.cpp
+++ C++/1016.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 
+using namespace std;
+
 int main() {
     //a cada 2 min o carro percorre 1km (Movimento Uniforme)
     //tempo = 2 * distância
+    float distancia = 0;
 
-    
+    cin >> distancia;
+
+    cout << distancia * 2 << " minutos\n";
 }
