a3a4d44bff27e98f82036c13502285db77b69bc7
bedreski <fabianabedreski@gmail.com>
2024-10-29 21:05:49 -0300

    1007 solved

---
+++ C++/1007.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b, c, d = 0;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+
+    cout << "DIFERENCA = " << (a * b - c * d) << endl;
+}
